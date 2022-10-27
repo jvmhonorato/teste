3c8d714828f7035336072bbb10c4488aedbda0e1
vituhonorato <vituhonorato.89@gmail.com>
2022-10-27 01:02:28 -0300

    test git

---
+++ index.js
@@ -0,0 +1,10 @@
+const obj = [
+    {nome: 'victor', email:'vituhonorato@gmail.com'},
+    {nome: 'Agda', email:'agda@gmail.com'}
+]
+obj.push({nome:'hann', email:'hann@gmail.com'})
+obj.push({nome:'neide', email:'neide@gmail.com  '})
+
+
+const result = obj
+console.log(result)
